606d04ed828d7ad6a03954d74f92439889b3138b
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-02 23:01:29 -0400

    fix on last digit task and permissions for the other files

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number >= 0:
+    last_digit = number % 10
+elif number < 0:
+    number = number * -1
+    last_digit = (number % 10) * -1
+    number = number * -1
 
-last_digit = abs(number) % 10
-
-print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
-    print("and is greater than 5")
+    str1 = 'and is greater than 5'
+    print(f'Last digit of {number} is {last_digit} {str1}')
 elif last_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+    str2 = 'and is 0'
+    print(f'Last digit of {number} is {last_digit} {str2}')
+elif last_digit < 6 and last_digit != 0:
+    str3 = 'and is less than 6 and not 0'
+    print(f'Last digit of {number} is {last_digit} {str3}')
