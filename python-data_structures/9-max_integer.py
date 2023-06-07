e918b6eb1266b0e04c0207e9814d3dfea4e02304
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-07 16:35:01 -0400

    fix task 9 file

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
-
 #!/usr/bin/python3
+
 
 def max_integer(my_list=[]):
     if my_list is None:
-        return None
+        my_list[0] = None
+        return my_list[0]
     my_list.sort()
     my_list.reverse()
     return my_list[0]
