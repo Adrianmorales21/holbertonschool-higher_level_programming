5e6659bc52d76bc8fa05ae9c86b92759f6bc5df0
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-07 12:32:04 -0400

    file 7 task fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,14 +3,18 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    new_tuple = ()
-    if tuple_a < 2:
-        tuple_a = 0,
-    elif tuple_a > 2:
-        tuple_a = tuple_a[:2],
-    if tuple_b < 0:
-        tuple_b = 0,
-    elif tuple_b > 2:
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+
+    elif len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    elif len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
-    new_tuple = tuple_a + tuple_b
+
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
     return new_tuple
