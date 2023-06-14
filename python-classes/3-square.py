873fe97e4de28d23688299b5bb894c79d25312cd
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-14 13:46:28 -0400

    task 3 file

---
+++ python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Creates new class called square
+"""
+
+
+class Square:
+    """
+    initializes the self variable
+    """
+
+    def __init__(self, size=0):
+        """
+        find error statements
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        """
+        Create private size instance
+        """
+        self.__size = size
+    """
+    method to calculate area
+    """
+
+    def area(self):
+
+        return self.__size * self.__size
