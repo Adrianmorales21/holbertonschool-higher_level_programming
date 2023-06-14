919bc616b3c0402d97769f562504ec240bf371a9
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-14 08:16:45 -0400

    comments to code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,5 +5,12 @@ Creates new class called square
 
 
 class Square:
+    """
+    initializes the self variable
+    """
+
     def __init__(self):
+        """
+        passes to the end of file
+        """
         pass
