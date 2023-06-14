cdbf1d3446d58f43f3d21979cb95327c706fed8f
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-14 08:35:18 -0400

    task 1 file

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Creates new class called square
+"""
+
+
+class Square:
+    """
+    initializes the self variable
+    """
+
+    def __init__(self, size):
+        """
+        create private size instance
+        """
+        self.__size = size
+        """
+        passes to the end of file
+        """
+        pass
