f3e5113c37e8e2faa96ca2ff742ad9eab38d0b8b
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-06-22 15:08:08 -0400

    task 1 test and module file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module to define MyList class"""
+
+
+class MyList(list):
+    """Class that inherits
+    """
+
+    def print_sorted(self):
+        """Prints the list in sorted order.
+        """
+        print(sorted(self))
