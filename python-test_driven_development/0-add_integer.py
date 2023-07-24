8031ed17e63d488eaf475ed6a1dcdc3cf36fada5
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-07-24 19:33:11 -0400

    readme file and task 1 files

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    module to sum 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+        Function that adds 2 intergers.
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+
+    if type(a) == float:
+        a = int(a)
+
+    if type(b) == float:
+        b = int(b)
+
+    return a + b
