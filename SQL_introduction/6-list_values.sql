ba9beb8e028f6b3663c2ec813fd4e268533118fc
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-07-26 22:24:04 -0400

    task 6 file fix

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,7 +1,2 @@
--- Script that lists all rows of
-
--- the table first_table from the database
-
---hbtn_0c_0 in your MySQL server
-
+-- list all rows of the table in database
 SELECT * FROM first_table;
